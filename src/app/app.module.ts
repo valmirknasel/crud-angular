64eed70e54fec214f8e9b234ba23d8f28dc14a72
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-19 16:05:02 -0400

    corrigida a importacao do CoursesModule ao inserir o <router-outlet> as NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
+import {AppRountingModule} from "./app-rounting.module";
 import {CoursesModule} from "./courses/courses.module";
 
 @NgModule({
@@ -14,6 +15,7 @@ import {CoursesModule} from "./courses/courses.module";
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    AppRountingModule,
     CoursesModule
   ],
   providers: [],
